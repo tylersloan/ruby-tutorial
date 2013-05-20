d078fb0688866db9f9700aa19acf5639dd5bed3d
Tyler Sloan <tyler.g.sloan@gmail.com>
2013-05-20 11:18:57 -0400

    Rails setip

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BabyTwitter::Application.config.secret_key_base = '47652a58a9df4f9e761274494a1a8caa092498c9166b82a36a660c40a611d9feafe89e02911aebeabc68ed00960e2311ceef66a89ec4a99129aa3e378fa5f495'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BabyTwitter::Application.config.session_store :cookie_store, key: '_baby_twitter_session'
